bc06b2d725b193351b9014843d8cd727539986ca
adityaarora <aditya.arora@lcbo.com>
2018-05-21 15:43:54 -0400

    fixed for organisation pages

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-(function () {
-  var dashboard = document.getElementsByClassName("dashboard")[0];
-  dashboard.appendChild(dashboard.children[0]);
-})()
+var dashboard = document.getElementsByClassName("dashboard")[0];
+dashboard.appendChild(dashboard.children[0]);
